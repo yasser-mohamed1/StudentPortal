b802ef0eadedcd8a5a6aaedbbeec20b72cb6769a
yasser-mohamed1 <y4ssermoh4med@gmail.com>
2024-04-04 20:32:27 +0200

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentPortal.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
